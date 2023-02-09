fce81a3a4f74c56cd25131cd40b0affb18e33224
Duarte Almeida <duartemalmeida@gmail.com>
2023-02-09 11:02:17 +0000

    move selected words logic to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,30 @@
 import Head from 'next/head';
-import styles from '@/styles/Home.module.css';
+import { useState, useEffect } from 'react';
+import styles from '@/styles/Home.module.scss';
+import { Board } from '@/components/Board/Board';
+import { getRandomWords } from '@/components/Board/helperFunctions';
+import { DifficultyLevelContext } from '@/context/context';
 
 export default function Home() {
+  const [words, setWords] = useState<string[]>([]);
+  const [currentWords, setCurrentWords] = useState<string[]>([]);
+  const [difficultyLevel, setDifficultyLevel] = useState(8);
+
+  useEffect(() => {
+    const loadWords = async () => {
+      const response = await fetch('/wordlist.txt');
+      const contents = await response.text();
+      setWords(contents.split('\n'));
+    };
+
+    loadWords();
+  }, []);
+
+  useEffect(() => {
+    if (words.length > 0)
+      setCurrentWords(getRandomWords(words, difficultyLevel));
+  }, [words, difficultyLevel]);
+
   return (
     <>
       <Head>
@@ -10,7 +33,11 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='' />
       </Head>
-      <main></main>
+      <main>
+        <DifficultyLevelContext.Provider value={difficultyLevel}>
+          <Board words={currentWords} />
+        </DifficultyLevelContext.Provider>
+      </main>
     </>
   );
 }
